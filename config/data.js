c53562b851cea7b317cd0de5d79547423a225e06
RFebrians <ekikz1997@gmail.com>
2021-07-25 21:59:46 +0700

    Moving Forward!

---
+++ config/data.js
@@ -0,0 +1,30 @@
+//Brown Wheats Snow - https://unsplash.com/photos/jGf0KBPj3x4
+
+
+export const data = [
+  {
+    id: '1',
+    title: 'Morning',
+    description: 'Soft Lighten Emmited a New Day',
+    image_url:
+      'https://images.unsplash.com/photo-1502447533750-9860c1269ae3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    iconName: 'cup'
+  },
+
+  {
+    id: '2',
+    title: 'Day',
+    description: 'The Wind Scent Leavin Trail',
+    image_url:
+      'https://images.unsplash.com/photo-1569699894831-ce89e09765ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80',
+    iconName: 'book-open'
+  },
+  {
+    id: '3',
+    title: 'Sunset ',
+    description: 'Resonance From Past , Into a Smiles',
+    image_url:
+      'https://images.unsplash.com/photo-1594842059196-5b6b8fa73187?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
+    iconName: 'present'
+  }
+];
---
+++ screens/DetailScreen.js
@@ -0,0 +1,163 @@
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Dimensions
+} from 'react-native';
+import { SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { SharedElement } from 'react-navigation-shared-element';
+import * as Animatable from 'react-native-animatable';
+
+const { height } = Dimensions.get('window');
+const ITEM_HEIGHT = height * 0.5;
+
+const DetailScreen = ({ navigation, route }) => {
+  const { item } = route.params;
+  const buttonRef = React.useRef();
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#0f0f0f' }}>
+      <SharedElement id={`item.${item.id}.image_url`}>
+        <Image
+          source={{ uri: item.image_url }}
+          style={{
+            width: '100%',
+            height: ITEM_HEIGHT,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20
+          }}
+          resizeMode='cover'
+        />
+      </SharedElement>
+      <Animatable.View
+        ref={buttonRef}
+        animation='fadeIn'
+        duration={600}
+        delay={300}
+        style={[StyleSheet.absoluteFillObject]}
+      >
+        <MaterialCommunityIcons
+          name='close'
+          size={28}
+          color='#fff'
+          style={{
+            position: 'absolute',
+            top: 40,
+            right: 20,
+            zIndex: 2
+          }}
+          onPress={() => {
+            buttonRef.current.fadeOut(100).then(() => {
+              navigation.goBack();
+            });
+          }}
+        />
+      </Animatable.View>
+      <View
+        style={{ flexDirection: 'row', marginTop: 10, paddingHorizontal: 20 }}
+      >
+        <SharedElement id={`item.${item.id}.iconName`}>
+          <SimpleLineIcons size={40} color='white' name={item.iconName} />
+        </SharedElement>
+        <View style={{ flexDirection: 'column', paddingLeft: 6 }}>
+          <SharedElement id={`item.${item.id}.title`}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 24,
+                fontWeight: 'bold',
+                lineHeight: 28
+              }}
+            >
+              {item.title}
+            </Text>
+          </SharedElement>
+          <SharedElement id={`item.${item.id}.description`}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 16,
+                fontWeight: 'bold',
+                lineHeight: 18
+              }}
+            >
+              {item.description}
+            </Text>
+          </SharedElement>
+        </View>
+      </View>
+      <ScrollView
+        indicatorStyle='white'
+        style={{
+          paddingHorizontal: 20,
+          backgroundColor: '#0f0f0f'
+        }}
+        contentContainerStyle={{ paddingVertical: 20 }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            color: '#fff',
+            lineHeight: 24,
+            marginBottom: 4
+          }}
+        >
+          Where ever you Go , May your heart lead into a straight path .
+
+To know , that we are wandering , through times , 
+into a beautiful journey for ever wandering .
+
+What was happen , is a better for you that reach to overcome it . 
+So strong , We are never preparin for this before , but somehow ... Who knows 
+        </Text>
+        
+        <Text
+          style={{
+            fontSize: 18,
+            color: '#fff',
+            lineHeight: 24,
+            marginBottom: 4
+          }}
+        >
+         
+          It is going Better , for the Ones who never givin up .
+          Through times ,we are leavin mark to rail and trails .
+          Day and Night are just yesterday .
+          Sailing The Ocean , Embrace the Wind , Overflowin the River. 
+
+        </Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+DetailScreen.sharedElements = route => {
+  const { item } = route.params;
+  return [
+    {
+      id: `item.${item.id}.image_url`,
+      animation: 'move',
+      resize: 'clip'
+    },
+    {
+      id: `item.${item.id}.title`,
+      animation: 'fade',
+      resize: 'clip'
+    },
+    {
+      id: `item.${item.id}.description`,
+      animation: 'fade',
+      resize: 'clip'
+    },
+    {
+      id: `item.${item.id}.iconName`,
+      animation: 'move',
+      resize: 'clip'
+    }
+  ];
+};
+
+export default DetailScreen;
